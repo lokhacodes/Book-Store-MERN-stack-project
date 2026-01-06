1ef9d545769bbaa23c85c50a3324480f196e49a3
lokhacodes <sumaiyasonali421@gmail.com>
2026-01-06 21:52:44 +0600

    set up server

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,10 @@ import { PORT } from './config.js';
 
 const app = express();
 
+app.get('/', (req, res) => {
+    console.log(res);
+    return res.status(200).send('Hello, World!');
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
