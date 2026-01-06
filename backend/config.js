6a03c8acf4af6b252bbb9bf00268b3f93917861e
lokhacodes <sumaiyasonali421@gmail.com>
2026-01-06 23:22:34 +0600

    connect to db

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,3 @@
 export const PORT = 5000;
+
+export const mongoDBURL = "mongodb+srv://sumaiyasonali421_db_user:123321@libm.csqtnve.mongodb.net/?appName=libM";
--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
-import { PORT } from './config.js';
+import { PORT, mongoDBURL } from './config.js';
+import mongoose from 'mongoose';
 
 const app = express();
 
@@ -8,6 +9,15 @@ app.get('/', (req, res) => {
     return res.status(200).send('Hello, World!');
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+
+
+
+mongoose
+    .connect(mongoDBURL)
+    .then(() => {
+        console.log('Connected to MongoDB');
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    })
+    .catch((err) => console.log(err));
