5a12f380ab5c9ed545d5f0feebacf46da164d054
lokhacodes <sumaiyasonali421@gmail.com>
2026-01-07 20:50:50 +0600

    create book schema

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,11 @@
 import express from 'express';
-import { PORT, mongoDBURL } from './.env';
+import { PORT, mongoDBURL } from './config.js';
 import mongoose from 'mongoose';
 
 const app = express();
 
 app.get('/', (req, res) => {
-    console.log(res);
+    
     return res.status(200).send('Hello, World!');
 });
 
