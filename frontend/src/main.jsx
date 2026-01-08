4528558eb45aff2c61cc5bc553c56c133ac370c6
lokhacodes <sumaiyasonali421@gmail.com>
2026-01-08 12:13:57 +0600

    SPA and Add react router dom

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,9 +6,9 @@ import { BrowserRouter } from 'react-router-dom';
 import { SnackbarProvider } from 'notistack';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-   <React.StrictMode>
+   <BrowserRouter>
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>,
 );
 
 
